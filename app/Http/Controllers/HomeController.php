d2b9a20a212f080929e11c24efde2e21e06f4177
negin-titanium <neginshahany@gmail.com>
2022-08-13 16:54:32 +0430

    add homeController and make the new route

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    public function redirect(){
+
+        if(Auth::id()){
+            //if  there is any user that logged in we check usertype
+            if(Auth::user()->usertype=='0'){
+                return view('dashboard');
+            }else{
+                return view('admin.home');
+            }
+        }else{
+            return redirect()->back();
+        }
+    }
+}
