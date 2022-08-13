4e78ff4f6f2fc8301f155dd3b0fa2b3134c509c5
negin-titanium <neginshahany@gmail.com>
2022-08-13 18:57:28 +0430

    add home route for non users at the homepage

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,10 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/home', [HomeController::class, 'redirect']);
+
 
 Route::middleware([
     'auth:sanctum',
