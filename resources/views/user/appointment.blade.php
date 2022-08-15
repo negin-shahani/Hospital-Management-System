5a6b171f178bf0e341f1651da3c0b4624027ad6e
negin-titanium <neginshahany@gmail.com>
2022-08-15 11:29:07 +0430

    add doctor option to appointment form

--- resources/views/user/appointment.blade.php
+++ resources/views/user/appointment.blade.php
@@ -15,11 +15,11 @@
           </div>
           <div class="col-12 col-sm-6 py-2 wow fadeInRight" data-wow-delay="300ms">
             <select name="departement" id="departement" class="custom-select">
-              <option value="general">General Health</option>
-              <option value="cardiology">Cardiology</option>
-              <option value="dental">Dental</option>
-              <option value="neurology">Neurology</option>
-              <option value="orthopaedics">Orthopaedics</option>
+              
+              @foreach($doctor as $doctors)
+              <option value="{{$doctors->name}}">{{$doctors->name}}</option>
+              @endforeach
+
             </select>
           </div>
           <div class="col-12 py-2 wow fadeInUp" data-wow-delay="300ms">
