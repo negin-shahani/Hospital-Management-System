f1af4e3c83d173144989baec6944e0cc6bd3bb44
negin-titanium <neginshahany@gmail.com>
2022-08-16 18:59:45 +0430

    last update

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -33,7 +33,7 @@
             <div class="site-info">
               <a href="#"><span class="mai-call text-primary"></span> +00 123 4455 6666</a>
               <span class="divider">|</span>
-              <a href="#"><span class="mai-mail text-primary"></span> mail@example.com</a>
+              <a href="#"><span class="mai-mail text-primary"></span> neginshahany@gmail.com</a>
             </div>
           </div>
           <div class="col-sm-4 text-right text-sm">
