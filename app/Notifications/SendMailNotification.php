aac8d0c8d34c09b936aba97085a8e7efbe902e77
negin-titanium <neginshahany@gmail.com>
2022-08-16 18:04:33 +0430

    add notification function

--- app/Notifications/SendMailNotification.php
+++ app/Notifications/SendMailNotification.php
@@ -11,14 +11,15 @@ class SendMailNotification extends Notification
 {
     use Queueable;
 
+    private $details;
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($details)
     {
-        //
+        $this->details = $details;
     }
 
     /**
@@ -41,9 +42,10 @@ class SendMailNotification extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
+                    ->greeting($this->details['greeting'])
+                    ->line($this->details['body'])
+                    ->action($this->details['actiontext'], $this->details['actionurl'])
+                    ->line($this->details['endpart']);
     }
 
     /**
